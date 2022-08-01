692b45159762505d7679a8b4300413337f461236
Bhavyatyagi1730 <bhavyatyagi966@gmail.com>
2022-08-01 22:28:29 +0530

    hey this my project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,140 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState,useEffect} from 'react'
+import NumberFormat from 'react-number-format'
+import Nav from './Nav';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const[ preState, setPreState]  = useState("")
+  const[ curstate, setCurstate]  = useState("")
+  const[input, setInput] = useState("0")
+  const[Operator, setOperator] = useState(null)
+  const[total, setTotal] = useState(false)
+
+  const inputNum = (e) => {
+   if(curstate.includes(".") && e.target.innerText === ".") return;
+    if(total){
+      setPreState("");
+    }
+
+    curstate ? setCurstate((pre) => pre + e.target.innerText) : setCurstate(e.target.innerText);
+    setTotal(false);
+  };
+
+  useEffect(() => {
+    setInput(curstate);
+  },[curstate]);
+  useEffect(() => {
+    setInput("0");
+  },[]);
+
+  const operator = (e) => {
+    setTotal(false)
+    setOperator(e.target.innerText)
+    if(curstate === "") return
+    if(preState !== ""){
+      equals()
+
+    }
+    else {
+      setPreState(curstate);
+      setCurstate("")
+    }
+
+
+  }
+  const equals = (e) => {
+    if(e?.target.innerText === "="){
+    setTotal(true)
+    };
+  let cal 
+  switch(Operator){
+    case "/":
+      cal = String(parseFloat(preState)/ parseFloat(curstate));
+         break;
+      case "+":
+        cal = String(parseFloat(preState) + parseFloat(curstate));
+        break;
+        case "*":
+          cal = String(parseFloat(preState) * parseFloat(curstate));
+          console.log("hello i m cal"+cal)
+          break;
+        case "-":
+          cal = String(parseFloat(preState) - parseFloat(curstate));
+          break;
+          default: return
+  }
+  setInput("");
+  setPreState(cal);
+  setCurstate("");
+  }
+  const percent = () => {
+preState ? setCurstate(String(parseFloat(curstate) / 100 * preState)) : setCurstate(String(parseFloat(curstate) / 100));
+  }
+  const reset = () => {
+    setPreState("");
+    setCurstate("");
+    setInput("0");
+  };
+
+   return (
+<div className='app'> 
+<Nav/>
+
+    <div className="container">
+   
+      <div className='wrapper'>
+        <div className='screen'>{input !== "" || input === "0" ? (
+        <NumberFormat 
+        value={input} 
+        displayType={"text"} 
+        thousandSeparator = {true} 
+        /> 
+       ) : (
+        
+        <NumberFormat
+         value={preState} 
+         displayType={"text"} 
+         thousandSeparator = {true}
+         />
+        )}
+        </div>
+        <div className='btn light-gray' onClick={reset}>AC</div>
+        <div className='btn light-gray' onClick={percent}>%</div>
+        
+        <div className='btn orange' onClick={operator}>/</div>
+  
+        <div className='btn orange' onClick={operator}>*</div>
+        <div className='btn' onClick={inputNum}>7</div>
+        <div className='btn' onClick={inputNum}>8</div>
+        <div className='btn' onClick={inputNum}>9</div>
+
+        <div className='btn orange' onClick={operator}>+</div>
+        <div className='btn' onClick={inputNum}>4</div>
+        <div className='btn' onClick={inputNum}>5</div>
+        <div className='btn' onClick={inputNum}>6</div>
+        
+        <div className='btn orange' onClick={operator}>-</div>
+        <div className='btn' onClick={inputNum}>1</div>
+        <div className='btn' onClick={inputNum}>2</div>
+        <div className='btn' onClick={inputNum}>3</div>
+   
+        <div className='btn' onClick={inputNum}>.</div>
+        <div className='btn zero' onClick={inputNum}>0</div>
+        <div className='btn' onClick={inputNum}>#</div>
+        <div className='btn' onClick={equals}>=</div>
+    
+
+
+
+
+        
+
+      </div>
+        
+    </div>
     </div>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./App.css"
+const Nav = () => {
+  return (
+    <div className='topnav'>
+
+        <img src='https://media-exp2.licdn.com/dms/image/C4D0BAQG-XkOD-nqQLw/company-logo_100_100/0/1656401897654?e=1666224000&v=beta&t=hW_-s6AZ3Uu_AF-xUD0z-24L37ZKlP9lnlJWtXL4IxA' alt='img'/>
+      
+         <h2>   CALCULATOR </h2>
+         
+         </div>
+  )
+}
+
+export default Nav
